7739148aa53cb3850743af73e1f6df7ab5f5fa74
Tyler Audette <tyleraudette@yahoo.com>
2015-04-17 15:40:11 -0600

    d

--- main.js
+++ main.js
@@ -8,14 +8,33 @@ $(document).on('ready', function() {
 		$(".hide").show();
 	});
 
+	var newName2 = function(){
+		console.log($(".Name").val());
+	};
+
 	$(".sub-button").click(function(event){
 		event.preventDefault();
-		console.log("test;");
-		console.log($(".Name").val());
-		$("newname").val("hello");
+		// CHANGE NAME
+		var nameChange = ($(".Name").val());
+		$(".newName").val(nameChange);
+
+		// CHANGE BIO
+		var bioChange = ($(".Bio").val());
+		$(".newBio").val(bioChange);
+
+		// CHANGE BOOK
+		var booksChange = ($(".Books").val());
+		$(".newBooks").val(booksChange);
+
+		// CHANGE JSS
+		var jsLibChange = ($(".jsLib").val());
+		$(".newjsLib").val(jsLibChange);
+
+		$(".hide").hide();
 
 
-		// Name.val logs the value of the class "Name" from the input list
+
+	// Name.val logs the value of the class "Name" from the input list
 		
 	});
 
